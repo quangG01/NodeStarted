49fbb89f26078e1247449cf8ca220e2312d8204a
quangG01 <quydang1404@gmail.com>
2023-07-17 15:39:23 +0700

    new learning

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
-const http = require('http'); 
-// const expressJS = require('express')
-function handleRequest(request,response){
-    //repquest
-    if( request.url === '/currenttime' )
-    {
-        response.statusCode = 200;
-        response.end('<h1>' +  Date().toISOString() + '</h1>');
-    } else if (request.url==='/')
-    {
-        response.statusCode = 200;
-        response.end('<h1>aaaaaaHello World</h1>')
-    }
-    
-    //lovslhost:4000/currenttime
-    
-}
+const express = require("express");
 
-const server = http.createServer(handleRequest);
+const app = express();
+app.use(express.urlencoded({extended:false}));
 
-server.listen(5000); 
+app.get('/',function(require,response){
+    response.send('<form action ="/store" method = "POST"><label>Ten : </label> <input name = "userName" type = "text"></input> <button type = "submit">submit</button> <form>');
+})
+
+app.post('/store',function(require,response){
+    let name = require.body.userName;
+    response.send(`<h1>ten la ${name}</h1>`);
+})
+app.listen(5000);
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+
+// const app = express();
+
+// app.use(express.urlencoded({extended:false}));
+
+// app.get('/currenttime',function(request,response){
+//     response.send('<h1>' + new Date().toISOString() + '</h1>');
+// })
+
+// app.get('/',function(request,response){
+//     response.send('<form action="/store-users" method = "POST"><label>hiaaa</label><input  name = "userName" type="text"></input><button type="submit">submit</button></form>');
+// })
+
+// app.post('/store-users' , function(request,response){
+//     const name  = request.body.userName;
+//     console.log(name);
+//     response.send(`<h1>user name stored ${name}</h1>`)
+// })
+
+// app.listen(2000);
+
